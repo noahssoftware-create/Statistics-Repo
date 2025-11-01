39cf58e6c6af750a798985f8cc2d5065ee152e7b
noahssoftware-create <noahs.software@gmail.com>
2025-11-01 08:42:09 -0700

    Clean up z score calculator

--- Z_Score_Calculator.py
+++ Z_Score_Calculator.py
@@ -1,11 +1,13 @@
-#create a z score calculator
+#Create variables inside Z score calculator
 data_point = float(input("Individual value: "))
 mu = float(input("Population mean: "))
 std = float(input("Standard deviation: "))
 
+# Add Z score calculation
 def z_Score(data_point, mu, std):
     z = (data_point - mu) / std
     return z
 
+# Print
 results = z_Score(data_point,mu,std)
 print(f"Z Score: {results}")
