95fe71860122147844e19616509d62ec7099ca7e
noahssoftware-create <noahs.software@gmail.com>
2025-11-03 15:01:08 -0700

    Imort csv and add some rounding into the z score to track the location of the Z Table

--- Z_Score_Calculator.py
+++ Z_Score_Calculator.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+#import csv for z table
+ztable_df = pd.read_csv("ztable_stats.csv")
+
 #Create variables inside Z score calculator
 data_point = float(input("Individual value: "))
 mu = float(input("Population mean: "))
@@ -10,7 +13,10 @@ def z_Score(data_point, mu, std):
     z = (data_point - mu) / std
     return z
 
-# Print
-results = z_Score(data_point,mu,std)
-print(f"Z Score: {results}")
+z_Score = z_Score(data_point,mu,std)
 
+
+# Find the column& row for z  table data frame
+rounded_z_score = round(float(z_Score,2))
+column_value = round(float (rounded_z_score)[-1:])
+row_value = round(float(rounded_z_score,1))
