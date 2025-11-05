50ec80aff4feddbafc6e6a294f87243b469e9a8c
noahssoftware-create <noahs.software@gmail.com>
2025-11-04 18:11:12 -0700

    Find Column and Row in Z tablecsv

--- Z_Score_Calculator.py
+++ Z_Score_Calculator.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from decimal import *
 import numpy as np
-#import csv for z table
+
+# Import csv for z table
 ztable_df = pd.read_csv("ztable_stats.csv")
 
-#Create variables inside Z score calculator
+# Create variables inside Z score calculator
 data_point = float(input("Individual value: "))
 mu = float(input("Population mean: "))
 std = float(input("Standard deviation: "))
@@ -14,10 +15,17 @@ def z_Score(data_point, mu, std):
     z = (data_point - mu) / std
     return z
 
+# Round Z score to the 2nd decimal
 z_Score = z_Score(data_point,mu,std)
 z_Score = round(z_Score,2)
 
 # Find the column& row for z  table data frame
-row_column = np.trunc(z_Score * 100)
-row_column = (row_column / 100)
-print(f"Z score: {row_column}")
+ztable_row = (z_Score * 10)
+ztable_row = (np.trunc(ztable_row))/10
+
+ztable_column = (ztable_row - z_Score)
+ztable_column = round(ztable_column,2)
+
+
+# Print Z Score and Probability of getting that value
+print(f"Z score: {z_Score}")
