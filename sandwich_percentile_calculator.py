97aced16de830f3726cf1fa85e84a3c592203459
noahssoftware-create <noahs.software@gmail.com>
2025-11-11 15:58:47 -0700

    Clean up Folder

--- sandwich_percentile_calculator.py
+++
@@ -1,9 +0,0 @@
-# Find the percentage of data between 2 points on a standard distribution
-x1 = float(input("Enter First X Value: "))
-x2 = float(input("Enter Second X Value: "))
-mu = float(input("Populations mean: "))
-std = float(input("Standard deviation: "))
-
-# Z score calculator lower score
-def z_score_calculator_lower(x1,mu,std):
-    z = (x1-mu)/std
